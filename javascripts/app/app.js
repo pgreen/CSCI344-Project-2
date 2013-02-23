02994a2e34e75c202a75b3763103702cfa9427e4
Penny Green <pgreen@unca.edu>
2013-02-23 14:23:56 -0500

    added the start and stop buttons and added script to only load when start is clicked.  stop re-loads the page.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -8,17 +8,17 @@ function startTweet() {
 function tweetFunction() {
   var twitter = new ctwitter.CTwitter();
   var countTweet = 1;
-  twitter.stream("statuses/filter", {lang:"en", track:["Ruby On Rails","Node.js", "Oracle SQL"]
+  twitter.stream("statuses/filter", {lang:"en", track:[$("#user_input").val()]
   }, function(stream){
     stream.on("data", function(tweet){
       if(countTweet <= 5){
         $('#' + countTweet).html('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
-        countTweet = countTweet + 1;
+          countTweet = countTweet + 1;
       } else {
-        $('div:first').fadeOut(500, function() {
-          $('div:first').remove();
-          $('body').append('<div class="hide"><p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p></div>');
-          $('div:last').fadeIn(500);
+        $('p:first').fadeOut(500, function() {
+          $('p:first').remove();
+          $('body').append('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p></div>');
+          $('p:last').fadeIn(500);
         });
       countTweet = countTweet + 1;
       } //esle
