94da5c11adcf4dfcedad57c24658c317a065eaec
Penny Green <pgreen@unca.edu>
2013-02-27 13:14:25 -0500

    changed the start and stop button functions to be inside of the main function. changed the button tag to a input tag to make everything work.

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,5 +1,4 @@
 var main = function(){
-  };//main
 
   var startTime = 0;
   var countTweet = 0;
@@ -8,49 +7,53 @@ var main = function(){
   var totalTime = 0;
   var myWord = /job|hiring|resume/g;
   var firstCount = 0;
+  
+  function startTweet() {
+    startTime = new Date();
+    tweetFunction();
+  }; //startTweet
 
-function startTweet() {
-  startTime = new Date();
-  tweetFunction();
-}; //startTweet
-
-function stopTweet() {
-  stopTime = new Date().getTime();
-  totalTime = stopTime - startTime;
-  if(firstCount === 0){
-    $('header').append('<h3>' + "From " + $.timeago(startTime) + " until now: " + countTweet + " total tweets, " +  countJob + " job related " + '</h3');
-    firstCount = firstCount + 1;
-  }else {
-    $('h3:first').fadeOut(500, function() {
+  function stopTweet() {
+    stopTime = new Date().getTime();
+    totalTime = stopTime - startTime;
+    if(firstCount === 0){
+      $('header').append('<h3>' + "From " + $.timeago(startTime) + " until now: " + countTweet + " total tweets, " +  countJob + " job related " + '</h3');
+      firstCount = firstCount + 1;
+    }else {
+      $('h3:first').fadeOut(500, function() {
         $('h3:first').remove();
         $('header').append('<h3>' + "From " + $.timeago(startTime - totalTime) + " until now: " + countTweet + " total tweets, " +  countJob + " job related " + '</h3');
         $('h3:last').fadeIn(500);
-      });//fading out and fading in
+        });//fading out and fading in
     }//else
   };// stopTweet
 
-function tweetFunction() {
-  var twitter = new ctwitter.CTwitter();
-  countTweet = 1;
-  twitter.stream("statuses/filter", {lang:"en", track:[$("#user_input").val()]
-  }, function(stream){
-    stream.on("data", function(tweet){
-      if(countTweet <= 5){
-        $('#' + countTweet).html('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
+  function tweetFunction() {
+    var twitter = new ctwitter.CTwitter();
+    countTweet = 1;
+    twitter.stream("statuses/filter", {lang:"en", track:[$("#user_input").val()]
+    }, function(stream){
+      stream.on("data", function(tweet){
+        if(countTweet <= 5){
+          $('#' + countTweet).html('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
           countTweet = countTweet + 1;
-      } else {
-        $('p:first').fadeOut(500, function() {
-          $('p:first').remove();
-          $('article').append('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
-          $('p:last').fadeIn(500);
-        });//removeing the first one and fading in the last one
-        countTweet = countTweet + 1;
+        } else {
+          $('p:first').fadeOut(500, function() {
+            $('p:first').remove();
+            $('article').append('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
+            $('p:last').fadeIn(500);
+          });//removeing the first one and fading in the last one
+          countTweet = countTweet + 1;
+        } //else
         if(myWord.test(tweet.text)) {
           countJob = countJob + 1;
         }//test for job keywords
-      } //else
-    }); //tweet
-  }); //stream
-}; //tweetFunction
+      }); //tweet
+    }); //stream
+  }; //tweetFunction
+
+  $("#start-click").click(startTweet);
+  $("#stop-click").click(stopTweet);
+};//main
 
 $(document).ready(main);
