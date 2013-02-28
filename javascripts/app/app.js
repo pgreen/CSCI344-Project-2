39eb33f0311c026f47ed6ee4349a03e506860f12
Penny Green <pgreen@unca.edu>
2013-02-27 21:21:50 -0500

    changed due to running code through htmllint and jslint

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,59 +1,62 @@
-var main = function(){
+/*global $*/
+var main = function () {
+    "use strict";
+    var $ = window.$,
+        ctwitter = window.ctwitter,
+        startTime = 0,
+        countTweet = 0,
+        countJob = 0,
+        stopTime = 0,
+        totalTime = 0,
+        firstCount = 0,
+        myWord = /job|hiring|resume/g;
+    function tweetFunction() {
+        var twitter = new ctwitter.CTwitter();
+        countTweet = 1;
+        twitter.stream("statuses/filter", {lang: "en", track: [$("#user_input").val()]
+            }, function (stream) {
+            stream.on("data", function (tweet) {
+                if (countTweet <= 10) {
+                    $('#' + countTweet).html('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
+                    countTweet = countTweet + 1;
+                } else {
+                    $('p:first').fadeOut(500, function () {
+                        $('p:first').remove();
+                        $('article').append('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
+                        $('p:last').fadeIn(500);
+                    });//removeing the first one and fading in the last one
+                    countTweet = countTweet + 1;
+                } //else
+                if (myWord.test(tweet.text)) {
+                    countJob = countJob + 1;
+                }//test for job keywords
+            }); //tweet
+        }); //stream
+    }  //tweetFunction
 
-  var startTime = 0;
-  var countTweet = 0;
-  var countJob = 0;
-  var stopTime = 0;
-  var totalTime = 0;
-  var myWord = /job|hiring|resume/g;
-  var firstCount = 0;
-  
-  function startTweet() {
-    startTime = new Date();
-    tweetFunction();
-  }; //startTweet
+    function startTweet() {
+        startTime = new Date();
+        tweetFunction();
+    }  //startTweet
 
-  function stopTweet() {
-    stopTime = new Date().getTime();
-    totalTime = stopTime - startTime;
-    if(firstCount === 0){
-      $('header').append('<h3>' + "From " + $.timeago(startTime) + " until now: " + countTweet + " total tweets, " +  countJob + " job related " + '</h3');
-      firstCount = firstCount + 1;
-    }else {
-      $('h3:first').fadeOut(500, function() {
-        $('h3:first').remove();
-        $('header').append('<h3>' + "From " + $.timeago(startTime - totalTime) + " until now: " + countTweet + " total tweets, " +  countJob + " job related " + '</h3');
-        $('h3:last').fadeIn(500);
-        });//fading out and fading in
-    }//else
-  };// stopTweet
-
-  function tweetFunction() {
-    var twitter = new ctwitter.CTwitter();
-    countTweet = 1;
-    twitter.stream("statuses/filter", {lang:"en", track:[$("#user_input").val()]
-    }, function(stream){
-      stream.on("data", function(tweet){
-        if(countTweet <= 5){
-          $('#' + countTweet).html('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
-          countTweet = countTweet + 1;
+    function stopTweet() {
+        stopTime = new Date().getTime();
+        totalTime = stopTime - startTime;
+        if (firstCount === 0) {
+            $('header').append('<h3>' + "From " + $.timeago(startTime) + " until now: " + countTweet + " total tweets, " +  countJob + " job related " + '</h3');
+            firstCount = firstCount + 1;
         } else {
-          $('p:first').fadeOut(500, function() {
-            $('p:first').remove();
-            $('article').append('<p><img src="' + tweet.profile_image_url + '" />' + tweet.text + '</p>');
-            $('p:last').fadeIn(500);
-          });//removeing the first one and fading in the last one
-          countTweet = countTweet + 1;
-        } //else
-        if(myWord.test(tweet.text)) {
-          countJob = countJob + 1;
-        }//test for job keywords
-      }); //tweet
-    }); //stream
-  }; //tweetFunction
+            $('h3:first').fadeOut(500, function () {
+                $('h3:first').remove();
+                $('header').append('<h3>' + "From " + $.timeago(startTime - totalTime) + " until now: " + countTweet + " total tweets, " +  countJob + " job related " + '</h3');
+                $('h3:last').fadeIn(500);
+            });//fading out and fading in
+        }//else
+    }// stopTweet
 
-  $("#start-click").click(startTweet);
-  $("#stop-click").click(stopTweet);
+
+    $("#start-click").click(startTweet);
+    $("#stop-click").click(stopTweet);
 };//main
 
 $(document).ready(main);
